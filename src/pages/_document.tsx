1390c3426deb51598db61b26db313b20749d1613
Murilo <murilo.messias@gmail.com>
2021-04-20 18:24:05 -0300

    :tada: first config [chakraui +  typescript]

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocuments extends Document {
+  render() {
+    return (
+      <Html>
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
